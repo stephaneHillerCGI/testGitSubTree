b0bd6df3d9129c7bfe8036a6c4a9892f4ca54315
Hiller <stephane.hiller@cgi.com>
2019-03-13 16:21:18 +0100

    tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MyModule, MyProvider } from '../mySubTree/commonModule/src';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MyModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MyProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MyProvider } from '../../mySubTree/commonModule/src';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private myProvider: MyProvider
+    ) {
 
   }
 
+
+  ngOnInit() {
+    this.myProvider.myMethod();
+    this.navCtrl.push("MyPage");
+  }
 }
